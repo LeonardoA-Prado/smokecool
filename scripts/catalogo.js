4c34ad7a66922e3b62693a92696c8d30d1c56898
LilSeidel <119064389+KevinSoto9@users.noreply.github.com>
2023-04-29 14:39:41 +0200

    Update catalogo.js

--- scripts/catalogo.js
+++ scripts/catalogo.js
@@ -9,6 +9,14 @@ function CrearCatalogo(){
     const contenedorTablas = document.createElement("div");
     contenedorTablas.classList.add("contenedorTabla");
 
+    //Link Carrito de compra
+    const carritoLink = document.createElement("a");
+    carritoLink.classList.add("carritoLogoLink")
+   
+    const carritoLinkFoto = document.createElement("img");
+    carritoLinkFoto.classList.add("carritoFoto")
+    carritoLinkFoto.src = "images/107831.png";
+
     //Titulo para el catalogo de sabores de cachimbas
     const nombreSabores = document.createElement("p");
     nombreSabores.textContent = 'Sabores de cachimba';
@@ -33,6 +41,9 @@ function CrearCatalogo(){
     const saboresCantidad = document.createElement("th");
     saboresCantidad.textContent = "Cantidad";
 
+    const saboresBoton = document.createElement("th");
+    saboresBoton.textContent = "Comprar"
+
     //Segunda fila (Mango)
     const saborMango = document.createElement("tr");
     saborMango.classList.add("estiloProductos");
@@ -50,21 +61,68 @@ function CrearCatalogo(){
     descripciónMango.textContent = "Sabor de Cachimba de Mango Brasileño (250 g)";
 
     const idMango = document.createElement("p");
-    idMango.textContent = "ID : F38"
+    idMango.textContent = "ID : F38";
 
     const saborMangoPvP = document.createElement("td");
-    saborMangoPvP.classList.add("PvP")
+    saborMangoPvP.classList.add("PvP");
 
     const PvPMango = document.createElement("p");
     PvPMango.textContent = "150€ ";
 
     const saborMangoCantidad = document.createElement("td");
-    saborMangoCantidad.classList.add("cantidad")
+    saborMangoCantidad.classList.add("cantidad");
 
-    const cantidadMangoContador = document.createElement("input")
-    cantidadMangoContador.setAttribute("type","number")
+    const cantidadMangoContador = document.createElement("input");
+    cantidadMangoContador.setAttribute("type","number");
 
-    //Tercera fila (Pistacho) Estoy en ello todavia
+    const saborMangoBoton = document.createElement("td");
+    saborMangoBoton.classList.add("botones")
+
+    const botonMango = document.createElement("button");
+    botonMango.textContent = "Añadir al carrito de la compra";
+
+
+    //Tercera fila (Pistacho) 
+
+    const saborPistacho = document.createElement("tr");
+    saborPistacho.classList.add("estiloProductos");
+
+    const saborPistachoFoto = document.createElement("td");
+
+    const fotoPistacho = document.createElement("img");
+    fotoPistacho.classList.add("fotoProductos");
+    fotoPistacho.src = "images/250g-pistachio-kiss_1.jpg";
+
+    const saborPistachoDescripción= document.createElement("td");
+    saborPistachoDescripción.classList.add("descripciones");
+    
+    const descripciónPistacho = document.createElement("p");
+    descripciónPistacho.textContent = "Sabor de Cachimba de Pistacho Picante (250 g)";
+
+    const idPistacho= document.createElement("p");
+    idPistacho.textContent = "ID : A81";
+
+    const saborPistachoPvP = document.createElement("td");
+    saborPistachoPvP.classList.add("PvP");
+
+    const PvPPistacho = document.createElement("p");
+    PvPPistacho.textContent = "23€ ";
+
+    const saborPistachoCantidad = document.createElement("td");
+    saborPistachoCantidad.classList.add("cantidad");
+
+    const cantidadPistachoContador = document.createElement("input");
+    cantidadPistachoContador.setAttribute("type","number");
+
+    const saborPistachoBoton = document.createElement("td");
+    saborPistachoBoton.classList.add("botones")
+
+    const botonPistacho = document.createElement("button");
+    botonPistacho.textContent = "Añadir al carrito de la compra";
+
+    //Implementar Carrito Logo
+
+    carritoLink.appendChild(carritoLinkFoto);
 
     //Implementar el indice
 
@@ -72,6 +130,7 @@ function CrearCatalogo(){
     indiceSabores.appendChild(saboresDescripción);
     indiceSabores.appendChild(saboresPvP);
     indiceSabores.appendChild(saboresCantidad);
+    indiceSabores.appendChild(saboresBoton)
 
     //Implementar Mango
 
@@ -80,19 +139,38 @@ function CrearCatalogo(){
     saborMangoDescripción.appendChild(idMango)
     saborMangoPvP.appendChild(PvPMango);
     saborMangoCantidad.appendChild(cantidadMangoContador);
+    saborMangoBoton.appendChild(botonMango);
 
     saborMango.appendChild(saborMangoFoto);
     saborMango.appendChild(saborMangoDescripción);
     saborMango.appendChild(saborMangoPvP);
     saborMango.appendChild(saborMangoCantidad);
+    saborMango.appendChild(saborMangoBoton);
+
+     //Implementar Pistacho
+
+     saborPistachoFoto.appendChild(fotoPistacho);
+     saborPistachoDescripción.appendChild(descripciónPistacho);
+     saborPistachoDescripción.appendChild(idPistacho)
+     saborPistachoPvP.appendChild(PvPPistacho);
+     saborPistachoCantidad.appendChild(cantidadPistachoContador);
+     saborPistachoBoton.appendChild(botonPistacho);
+ 
+     saborPistacho.appendChild(saborPistachoFoto);
+     saborPistacho.appendChild(saborPistachoDescripción);
+     saborPistacho.appendChild(saborPistachoPvP);
+     saborPistacho.appendChild(saborPistachoCantidad);
+     saborPistacho.appendChild(saborPistachoBoton);
 
     //Implementar filasSabores
 
     tablaSabores.appendChild(indiceSabores);
-    tablaSabores.appendChild(saborMango)
+    tablaSabores.appendChild(saborMango);
+    tablaSabores.appendChild(saborPistacho);
 
     //Implementar tablas
 
+   
     contenedorTablas.appendChild(nombreSabores);
     contenedorTablas.appendChild(tablaSabores)
     catalogoContainer.appendChild(contenedorTablas);
