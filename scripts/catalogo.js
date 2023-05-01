8167ec5266ec110b078a37edcb4d2d947e273fad
KikoRiveraOficial <albertoywilly@gmail.com>
2023-05-01 13:33:18 +0200

    Array para tabla(incompleto)

--- scripts/catalogo.js
+++ scripts/catalogo.js
@@ -10,3 +10,20 @@ buscador.addEventListener("input", function () {
         fila.style.display = coincide ? "" : "none";
     });
 });
+
+const tabla = document.getElementsByTagName('table')[0];
+const filasTabla = tabla.querySelectorAll('tr');
+
+const datos = [];
+
+for (let i = 1; i < filasTabla.length; i++) {
+  const celdas = filas[i].querySelectorAll('td');
+  const descripcion = celdas[1].querySelector('.descripcion').textContent;
+  const id = celdas[1].querySelector('.id').textContent;
+  const precio = celdas[2].querySelector('.precio').textContent;
+  const cantidad = celdas[3].querySelector('.contador').value;
+  datos.push({descripcion, id, precio, cantidad});
+}
+
+console.log(datos);
+
