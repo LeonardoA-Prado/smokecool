3c150fbb70cfab8d6e4be475055658aa06f83cd5
Leo A. Prado <almeidaprado999@gmail.com>
2023-05-10 21:15:27 +0200

    Admin part 4 casi

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,17 +1,4 @@
-const form = document.querySelector('.formulario');
-form.addEventListener('submit', saveData);
 
-function saveData(event) {
-    event.preventDefault();
-    const email = document.getElementById('email').value;
-    const subject = document.getElementById('subject').value;
-    const message = document.getElementById('message').value;
-   
-    const data = {email, subject, message};
-
-    localStorage.setItem('formData', JSON.stringify(data));
-    
-}
 
 var abrirVentana = document.getElementById('admin'), 
     ventana = document.getElementById('ventana'),
