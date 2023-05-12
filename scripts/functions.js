5531f43ee250c4d1ecca0e30e2e46016795f48a6
LilSeidel <119064389+KevinSoto9@users.noreply.github.com>
2023-05-12 23:44:07 +0200

    Update functions.js

--- scripts/functions.js
+++ scripts/functions.js
@@ -28,3 +28,29 @@ if (passwordInput.value === '1234') {
     window.location.href = 'usuarios.html';
 }
 });
+
+const texts = {
+    Guillermo: "Guillermo de Molina Madrid(CEO del régimen judeo-masónico) Nacido en Campos do Jordão, Sao Paulo,Brasil, tengo 18 años y estoy estudiando actualmente un ciclo superior de Desarrollo de Aplicaciones Web. Puedo aportar creatividad a ideas de nuevos productos para la empresa ya que soy una persona a la que le gusta mucho el licor y siempre prueba nuevos sabores, al igual que con las cachimbas.",
+    Kevin: "Kevin Andrés Soto Briceño(CEO de Peru) Nacido en Lima, Perú, tengo 19 años y ahora mismo me encuentro estudiando 1º de Daw. Mi aportación a esta empresa es la contabilidad y los números que se manejan. Pero si hace falta vendo los datos de nuestros clientes para que la empresa no caiga en bancarrota.",
+    Leo: "Leonardo Almeida Prado(CEO de los Pingüinos) Nacido en Cuiabá, Mato Grosso,Brasil, tengo 19 años, actualmente estoy estudiando el grado superior de Desarrollo de Aplicaciones Web. Puedo aportar a la empresa la capacidad de planificar y organizar el trabajo para la gestión de productos o servicios, ventajas y riesgos(fumar mata).",
+    Antonio: "Antonio García Victoria(CEO del racismo) Nacido en Três Lagoas,Mato Grosso del Sur,Brasil, pero actualmente vivo en Marbella tengo 18 años y estoy estudiando 1º de Daw, lo que estoy dispuesto a darle a esta empresa es un análisis de mercado y los antecedentes que tenemos (pero no los penales, eso ya para otro día)"
+  };
+  
+  const buttons = document.querySelectorAll('.info-button');
+  const content = document.querySelector('.info-content');
+  
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const text = texts[button.getAttribute('data-text')];
+      content.textContent = text;
+    });
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+          buttons.forEach(otherButton => {
+            otherButton.classList.remove('selected');
+          });
+          button.classList.add('selected');
+        });
+      });
+  });
