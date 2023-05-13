46d4d2e19b9ea8df9ca96fff1103246ecb690daa
LilSeidel <119064389+KevinSoto9@users.noreply.github.com>
2023-05-13 19:51:30 +0200

    Update form.js

--- scripts/form.js
+++ scripts/form.js
@@ -12,3 +12,48 @@ function saveData(event) {
     localStorage.setItem('formData', JSON.stringify(data));
     
 }
+
+const miBoton = document.querySelector('#boton');
+const miNotificacion = document.querySelector('#noti');
+const miTitulo = document.querySelector('#notigrax');
+const miTexto = document.querySelector('#notires');
+
+miBoton.addEventListener('click', () => {
+  miTitulo.textContent = '¡Gracias por contactarnos!';
+  miTexto.textContent = 'Le enviaremos una respuesta al correo electronico';
+  miNotificacion.classList.add('mostrar');
+
+  setTimeout(() => {
+    miNotificacion.classList.remove('mostrar');
+  }, 2000);
+});
+
+const miInput = document.querySelector('#email');
+
+miInput.addEventListener('focus', () => {
+  miInput.classList.add('escribiendo');
+});
+
+miInput.addEventListener('blur', () => {
+  miInput.classList.remove('escribiendo');
+});
+
+const miInput2 = document.querySelector('#subject');
+
+miInput2.addEventListener('focus', () => {
+  miInput2.classList.add('escribiendo');
+});
+
+miInput2.addEventListener('blur', () => {
+  miInput2.classList.remove('escribiendo');
+});
+
+const miInput3 = document.querySelector('#message');
+
+miInput3.addEventListener('focus', () => {
+  miInput3.classList.add('escribiendo');
+});
+
+miInput3.addEventListener('blur', () => {
+  miInput3.classList.remove('escribiendo');
+});
