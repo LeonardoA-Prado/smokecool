c8e96ab2a7a157a36f7b82d7629f927f405cf9ed
LilSeidel <119064389+KevinSoto9@users.noreply.github.com>
2023-05-13 22:28:37 +0200

    Update functions.js

--- scripts/functions.js
+++ scripts/functions.js
@@ -63,3 +63,18 @@ const texts = {
     const ancho = 100 / numBarras;
     barra.style.width = `${ancho}%`;
     barra.style.height = `${valor}%`;});
+
+    var myIndex = 0;
+    SlideMove();
+    
+    function SlideMove() {
+      var i;
+      const  x = document.getElementsByClassName("slide");
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+      }
+      myIndex++;
+      if (myIndex > x.length) {myIndex = 1}    
+      x[myIndex-1].style.display = "block";  
+      setTimeout(SlideMove, 2500);    
+    }
