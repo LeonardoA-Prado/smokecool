1bd336cd456b085efbebd5ca4c11adf05af18a98
KikoRiveraOficial <114604297+KikoRivera777@users.noreply.github.com>
2023-05-18 18:19:43 +0200

    Empezar catalogo pal carrito puñetaaaa

---
+++ scripts/productos.js
@@ -0,0 +1,75 @@
+let productos = [
+    {
+        id: 1,
+        imagen: "",
+        nombre: 'Sabor de cachimba de mango',
+        precio: 20,
+        
+    },
+    {
+        id: 2,
+        imagen: "",
+        nombre: 'Sabor de cachimba de pistacho',
+        precio: 25,
+    },
+    {
+        id: 3,
+        imagen: "",
+        nombre: 'Sabor de cachimba de clorofila',
+        precio: 20,
+    },
+    {
+        id: 4,
+        imagen: "",
+        nombre: 'Sabor de cachimba de bacon',
+        precio: 20,
+    },
+    {
+        id: 5,
+        imagen: "",
+        nombre: 'Sabor de cachimba de marihuana',
+        precio: 28,
+    },
+    {
+        id: 6,
+        imagen: "",
+        nombre: 'Sabor de cachimba de love66',
+        precio: 17,
+    },
+    {
+        id: 7,
+        imagen: "",
+        nombre: 'Licor de maracuyá',
+        precio: 30,
+    },
+    {
+        id: 8,
+        imagen: "",
+        nombre: 'Licor de mandarina',
+        precio: 25,
+    },
+    {
+        id: 9,
+        imagen: "",
+        nombre: 'Licor de manzana',
+        precio: 25,
+    },
+    {
+        id: 10,
+        imagen: "",
+        nombre: 'Licor de coco de Sao Paulo',
+        precio: 35,
+    },
+    {
+        id: 11,
+        imagen: "",
+        nombre: 'Licor de pepinillo',
+        precio: 30,
+    },
+    {
+        id: 12,
+        imagen: "",
+        nombre: 'Licor de lagartija',
+        precio: 40,
+    }
+]
